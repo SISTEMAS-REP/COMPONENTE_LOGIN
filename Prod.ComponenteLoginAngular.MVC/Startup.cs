92da82d960adeeb2713aa0986f2e54d17627c5ec
Jesus Angel La Rosa Trinidad <ogti_temp84@produce.gob.pe>
2022-07-22 23:51:08 -0500

    prueba de envio

--- Prod.ComponenteLoginAngular.MVC/Startup.cs
+++ Prod.ComponenteLoginAngular.MVC/Startup.cs
@@ -16,6 +16,7 @@ namespace Prod.ComponenteLoginAngular.MVC
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
