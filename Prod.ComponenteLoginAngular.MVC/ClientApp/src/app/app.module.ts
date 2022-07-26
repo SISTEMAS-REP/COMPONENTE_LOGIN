e433b60fc90fdcf3cc79941a9bb082eb5ba5c6af
Jhoseph Ricardo Velasquez Vela <ogti_temp93@produce.gob.pe>
2022-07-26 03:28:47 -0500

    .

--- Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/app.module.ts
+++ Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
@@ -21,7 +20,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
