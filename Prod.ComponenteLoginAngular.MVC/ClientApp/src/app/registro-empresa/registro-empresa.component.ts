3d5d684e9359b52a3ce1ce7cd8ec4c83f70aca6f
Jhoseph Ricardo Velasquez Vela <ogti_temp93@produce.gob.pe>
2022-07-26 22:12:19 -0500

    avance

--- Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/registro-empresa/registro-empresa.component.ts
+++ Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/registro-empresa/registro-empresa.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroEmpresaComponent implements OnInit {
 
+  isVisiblePaso1 : boolean = true;
+  isVisiblePaso2 : boolean = false;
+  isVisiblePaso3 : boolean = false;
+  isVisiblePaso4 : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  clickPaso1 = () =>{
+    this.isVisiblePaso1 = true;
+    this.isVisiblePaso2 = false;
+  }
+  clickPaso2 = () =>{
+    this.isVisiblePaso1 = false;
+    this.isVisiblePaso2 = true;
+  }
+
+  clickPaso3 = () =>{
+    this.isVisiblePaso2 = false;
+    this.isVisiblePaso3 = true;
+  }
+
+  clickPaso4 = () =>{
+    this.isVisiblePaso3 = false;
+    this.isVisiblePaso4 = true;
+  }
+
 
 }
