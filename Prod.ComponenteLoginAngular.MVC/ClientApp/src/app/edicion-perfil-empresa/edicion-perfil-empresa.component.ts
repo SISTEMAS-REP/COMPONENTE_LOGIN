23309b8a00dbd4b6841c1ba6acb2aca192b54ec8
Jhoseph Ricardo Velasquez Vela <ogti_temp93@produce.gob.pe>
2022-07-31 18:18:50 -0500

    .

--- Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/edicion-perfil-empresa/edicion-perfil-empresa.component.ts
+++ Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/edicion-perfil-empresa/edicion-perfil-empresa.component.ts
@@ -6,10 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edicion-perfil-empresa.component.css']
 })
 export class EdicionPerfilEmpresaComponent implements OnInit {
+  isVisiblePerfil : boolean = true;
+  isVisibleRepresentante : boolean = true;
+  isVisibleContacto : boolean = true;
+
+  isVisibleEditarContacto : boolean = false;
+  isVisibleEditarRepresentante : boolean = false;
+  isVisibleEditarImg : boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clickEditarRepresentante = () =>{ 
+    this.isVisibleEditarRepresentante = true;
+    this.isVisibleRepresentante = false;  
+    this.isVisibleContacto = false; 
+  }
+
+  clickEditarContacto = () =>{ 
+    this.isVisibleEditarContacto = true;
+    this.isVisibleContacto = false; 
+    this.isVisibleRepresentante = false;    
+  }
+
+  clickEditarImg= () =>{ 
+    this.isVisibleEditarImg = true;
+    this.isVisiblePerfil = false;  
+    this.isVisibleEditarRepresentante = false; 
+    this.isVisibleRepresentante = false; 
+    this.isVisibleEditarContacto = false; 
+    this.isVisibleContacto = false;  
+  }
+
+  clickCancelarContacto= () =>{ 
+    this.isVisiblePerfil = true;  
+    this.isVisibleContacto = true; 
+    this.isVisibleEditarContacto = false;  
+  }
+
+  clickCancelarRepresentante= () =>{ 
+    this.isVisiblePerfil = true;  
+    this.isVisibleRepresentante = true; 
+    this.isVisibleEditarRepresentante = false; 
+  }
+
+  clickCancelarImg= () =>{ 
+    this.isVisiblePerfil = true;
+    this.isVisibleRepresentante  = true;
+    this.isVisibleContacto = true;
+    this.isVisibleEditarContacto = false; 
+    this.isVisibleEditarRepresentante = false; 
+    this.isVisibleEditarImg = false; 
+
   }
 
 }
--- Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/edicion-perfil-persona/edicion-perfil-persona.component.ts
+++ Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/edicion-perfil-persona/edicion-perfil-persona.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edicion-perfil-persona.component.css']
 })
 export class EdicionPerfilPersonaComponent implements OnInit {
+  isVisiblePerfil : boolean = true;
+  isVisibleContacto : boolean = true;
+  isVisibleEditarContacto : boolean = false;
+  isVisibleEditarImg : boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickEditarContacto = () =>{ 
+    this.isVisibleEditarContacto = true;
+    this.isVisibleContacto = false;   
+  }
+
+  clickEditarImg= () =>{ 
+    this.isVisibleEditarImg = true;
+    this.isVisiblePerfil = false;  
+    this.isVisibleEditarContacto = false; 
+    this.isVisibleContacto = false;  
+  }
+
+  clickCancelarContacto= () =>{ 
+    this.isVisiblePerfil = true;  
+    this.isVisibleContacto = true;  
+    this.isVisibleEditarContacto = false; 
+    this.isVisibleEditarImg = false;
+  }
+
+  clickCancelarImg= () =>{ 
+    this.isVisiblePerfil = true;  
+    this.isVisibleContacto = true;  
+    this.isVisibleEditarContacto = false;
+    this.isVisibleEditarImg = false; 
+
+  }
 }
