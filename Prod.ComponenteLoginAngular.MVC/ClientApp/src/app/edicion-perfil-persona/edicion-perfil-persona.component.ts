8b50465b849e0b41be2e2a4424d3d89f323625b3
Jhoseph Ricardo Velasquez Vela <ogti_temp93@produce.gob.pe>
2022-07-31 20:01:48 -0500

    .

--- Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/edicion-perfil-persona/edicion-perfil-persona.component.ts
+++ Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/edicion-perfil-persona/edicion-perfil-persona.component.ts
@@ -6,15 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edicion-perfil-persona.component.css']
 })
 export class EdicionPerfilPersonaComponent implements OnInit {
+  celular: string = null;
+  correo: string = null;
+
+
   isVisiblePerfil : boolean = true;
   isVisibleContacto : boolean = true;
   isVisibleEditarContacto : boolean = false;
   isVisibleEditarImg : boolean = false;
 
+  //Variables validador
+  validadorCelular :  boolean = false;
+  validadorCelularLength : boolean = false;
+  validadorCorreo :  boolean = false;
+  validadorCorreoInvalido : boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  btnGuardarContacto = () =>{
+    this.changeCelular();
+    this.changeCorreo();
+ 
+      if(!this.validadorCelular &&   !this.validadorCelularLength && !this.validadorCorreo && !this.validadorCorreoInvalido){
+        this.isVisiblePerfil = true;  
+        this.isVisibleContacto = true; 
+        this.isVisibleEditarContacto = false;  
+        this.limpiar();
+      }       
+  }
+
 
   clickEditarContacto = () =>{ 
     this.isVisibleEditarContacto = true;
@@ -32,7 +55,7 @@ export class EdicionPerfilPersonaComponent implements OnInit {
     this.isVisiblePerfil = true;  
     this.isVisibleContacto = true;  
     this.isVisibleEditarContacto = false; 
-    this.isVisibleEditarImg = false;
+    this.limpiar();
   }
 
   clickCancelarImg= () =>{ 
@@ -40,6 +63,90 @@ export class EdicionPerfilPersonaComponent implements OnInit {
     this.isVisibleContacto = true;  
     this.isVisibleEditarContacto = false;
     this.isVisibleEditarImg = false; 
+  }
+
+  changeCelular = () =>{
+    if(this.celular == null || this.celular == ""){
+      this.validadorCelular = true;
+      this.validadorCelularLength = false;
+    }
+    else{
+      if( this.celular.length < 9){    
+        this.validadorCelularLength = true;
+        this.validadorCelular = false;
+      }
+      else {
+        this.validadorCelular = false;
+        this.validadorCelularLength = false;
+      }     
+    }
+  }
+
+
+
+  changeCorreo = () =>{
+    if(this.correo == null || this.correo == ""){
+      this.validadorCorreo = true;
+      this.validadorCorreoInvalido = false;
+    }
+    else{
+      
+      var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      var regOficial = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  
+      if (reg.test(this.correo) && regOficial.test(this.correo)) {
+        this.validadorCorreoInvalido = false;
+        this.validadorCorreo = false;
+      } else if (reg.test(this.correo)) {
+        this.validadorCorreoInvalido = false;
+        this.validadorCorreo = false;
+  
+      } else {
+        this.validadorCorreoInvalido = true;
+        this.validadorCorreo = false;
+      }    
+    }
+  }
+
+
+  public restrictNumeric(e) {
+    let input;
+    if (e.metaKey || e.ctrlKey) {
+      return true;
+    }
+    if (e.which === 32) {
+     return false;
+    }
+    if (e.which === 0) {
+     return true;
+    }
+    if (e.which === 46) {
+      return true;
+     }
+    if (e.which < 33) {
+      return true;
+    }
+    if (e.which === 188){
+        return true;
+      }
+     
+
+    input = String.fromCharCode(e.which);
+    return !!/[\d\s]/.test(input);
+   }
+
+
+   limpiar =()=>
+  {
+    this.celular = null;
+    this.correo = null;
+
+    //Variables validador
+    this.validadorCelular = false;
+    this.validadorCelularLength = false;
+    this.validadorCorreo = false;
+    this.validadorCorreoInvalido = false;
 
   }
+
 }
