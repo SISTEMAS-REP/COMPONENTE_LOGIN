5d64958b10637c654498d5426709a6508266f840
Jesus Angel La Rosa Trinidad <ogti_temp84@produce.gob.pe>
2022-08-04 14:21:58 -0500

    .

---
+++ Prod.ComponenteLoginAngular.MVC/Controllers/SeguridadController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Prod.ComponenteLoginAngular.MVC.Controllers.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prod.ComponenteLoginAngular.MVC.Controllers
+{
+    [Route("[controller]")]
+    public class SeguridadController : CustomBaseController
+    {
+		public SeguridadController()
+		{
+			
+		}
+
+		[AllowAnonymous]
+		[HttpGet]
+		[Route("Recovery")]
+		public IActionResult Recovery([FromQuery] string Identificador, [FromQuery] string Code, [FromQuery] string Email, [FromQuery] string UserName)
+		{
+			//var user = this.GetUser();
+			ViewBag.IsAuthenticated = false;
+			if (User.Identity.IsAuthenticated)
+			{
+				ViewBag.IsAuthenticated = true;
+			}
+			ViewBag.Opc = "RC";
+			return View();
+		}
+
+	}
+}
