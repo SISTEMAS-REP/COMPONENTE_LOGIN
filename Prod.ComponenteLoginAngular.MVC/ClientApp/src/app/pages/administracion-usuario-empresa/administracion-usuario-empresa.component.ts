71df7ee6dce43e90ad96534b5593542c83edf2ef
Jhoseph Ricardo Velasquez Vela <ogti_temp93@produce.gob.pe>
2022-08-09 12:39:14 -0500

    .

--- Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/pages/administracion-usuario-empresa/administracion-usuario-empresa.component.ts
+++ Prod.ComponenteLoginAngular.MVC/ClientApp/src/app/pages/administracion-usuario-empresa/administracion-usuario-empresa.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administracion-usuario-empresa.component.css']
 })
 export class AdministracionUsuarioEmpresaComponent implements OnInit {
+  isVisiblePrincipal : boolean = true;
+  isVisibleEditarUsuario : boolean = false;
+  isVisibleAgregarUsuario : boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  BtnAgregarUsuario= () =>{ 
+    this.isVisibleAgregarUsuario = true;
+    this.isVisiblePrincipal = false;  
+    this.isVisibleEditarUsuario = false; 
+  }
+
+
+  BtnEditarUsuario= () =>{ 
+    this.isVisibleEditarUsuario = true; 
+    this.isVisibleAgregarUsuario = false;
+    this.isVisiblePrincipal = false;  
+  }
+
+  BtnCancelarContacto= () =>{ 
+    this.isVisiblePrincipal = true; 
+    this.isVisibleEditarUsuario = false;
+    this.isVisibleAgregarUsuario = false;  
+  }
+
 
 }
